a49ac41a395c32725dddd17f662c658dbef586ac
唐词伟 <tangciwei@bytedance.com>
2020-10-03 06:59:33 +0800

    feat: 3_react
    Change-Id: I006b996e50049d34799abae036ae825cb09a5da8

--- src/search.js
+++ src/search.js
@@ -1,22 +1,20 @@
-// 'use strict';
+"use strict";
 
-// import React from 'react';
-// import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 // import logo from './images/logo.png';
 // import './search.less';
 
-// class Search extends React.Component {
+class Search extends React.Component {
+  render() {
+    return (
+      <div className="search-text">
+        {
+          // 搜索文字的内容<img src={ logo } />
+        }
+      </div>
+    );
+  }
+}
 
-//     render() {
-//         return <div className="search-text">
-//             搜索文字的内容<img src={ logo } />
-//         </div>;
-//     }
-// }
-
-// ReactDOM.render(
-//     <Search />,
-//     document.getElementById('root')
-// );
-
-export function test() {}
+ReactDOM.render(<Search />, document.getElementById("root"));
