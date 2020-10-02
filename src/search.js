fd2e999ad31d31eb63f6fb29a1c334ab5aaa7c1d
唐词伟 <tangciwei@bytedance.com>
2020-10-03 07:03:58 +0800

    feat: 4_less
    Change-Id: I173b4a758615ccb53c2bb0fde587190085a254cc

--- src/search.js
+++ src/search.js
@@ -3,7 +3,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 // import logo from './images/logo.png';
-// import './search.less';
+import './search.less';
 
 class Search extends React.Component {
   render() {
