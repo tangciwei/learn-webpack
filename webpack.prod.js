ed205b72c63d6a9b6fadd74a5e3a401f2a932a3f
唐词伟 <tangciwei@bytedance.com>
2020-10-03 22:02:12 +0800

    feat: 23_errhandle
    Change-Id: Ia6d253ee8db5b731a9655553a254eca81b79b84a

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,23 +1,36 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // 无法和style-loader共存
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const cssnano = require('cssnano');
-const webpackBase = require('./webpack.base');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // 无法和style-loader共存
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const cssnano = require("cssnano");
+const webpackBase = require("./webpack.base");
 
 module.exports = {
   ...webpackBase,
-  mode: 'none',
+  mode: "none",
   plugins: [
     ...webpackBase.plugins,
     new MiniCssExtractPlugin({
-      filename: '[name].css',
+      filename: "[name].css",
     }),
     new OptimizeCSSAssetsPlugin({
       assetNameRegExp: /\.css$/g,
       cssProcessor: cssnano,
     }),
     new CleanWebpackPlugin(),
+    function () {
+      this.hooks.done.tap("done", (stats) => {
+        // console.log(stats)
+        // if (
+        //   stats.compilation.errors &&
+        //   stats.compilation.errors.length &&
+        //   process.argv.indexOf("--watch") == -1
+        // ) {
+        //   console.log("build error");
+        //   process.exit(1);
+        // }
+      });
+    },
   ],
 
   optimization: {
@@ -30,13 +43,13 @@ module.exports = {
         //   chunks: "all",
         // },
         commons: {
-          name: 'commons',
-          chunks: 'all',
+          name: "commons",
+          chunks: "all",
           minChunks: 2,
         },
       },
     },
   },
-  stats: 'errors-only'
+  stats: "errors-only",
   // devtool: "inline-source-map",
 };
