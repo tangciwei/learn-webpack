038506b65190d8d610b4a467963894dfaa9a0e93
唐词伟 <tangciwei@bytedance.com>
2020-10-03 16:51:26 +0800

    feat: 18_dynamicimport
    Change-Id: I9c34b36195239e80e5bb640a0f3cb7a4df799812

--- src/search/index.js
+++ src/search/index.js
@@ -4,20 +4,36 @@ import React from "react";
 import ReactDOM from "react-dom";
 import logo from "./images/logo.png";
 import "./search.less";
-import { a } from "../../common";
-a()
 
 class Search extends React.Component {
+  constructor() {
+    super(...arguments);
+
+    this.state = {
+      Text: null,
+    };
+  }
+
+  loadComponent() {
+    import("./text.js").then((Text) => {
+      this.setState({
+        Text: Text.default,
+      });
+    });
+  }
+
   render() {
+    const { Text } = this.state;
+    const addResult = "addResult";
     return (
       <div className="search-text">
-        223<img src={logo} />
+        {Text ? <Text /> : null}
+        {addResult}
+        搜索文字的内容
+        <img src={logo} onClick={this.loadComponent.bind(this)} />
       </div>
     );
   }
 }
-setTimeout(()=>{
-  ReactDOM.render(<Search />, document.getElementById('root'));
-})
-
+ReactDOM.render(<Search />, document.getElementById("root"));
 // ReactDOM.render(<Search />, document.body);
